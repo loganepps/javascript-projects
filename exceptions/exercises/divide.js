06eeba65e4bf1ea5e3f6d096b56c1c945adadf56
loganepps <logepps@gmail.com>
2024-07-26 16:11:38 -0500

    exceptions exercises

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,15 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+// Code your divide function here:
+let divide = function (numerator, denominator) {
+  if (denominator === 0) {
+    throw Error("Attempted to divide by zero.");
+  } else {
+    return numerator / denominator;
+  }
+};
 
 // Your function should return the result of numerator / denominator.
+console.log(divide(8, 2));
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
-
-// Code your divide function here:
+console.log(divide(6, 0));
